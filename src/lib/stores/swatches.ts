cfbfd0a6f5e7773b484c0ec50bc1bef486f22521
ccozens <ccozens@gmail.com>
2023-09-18 11:55:24 +0100

    updated colours so they all exist in P3

--- src/lib/stores/swatches.ts
+++ src/lib/stores/swatches.ts
@@ -1,16 +1,16 @@
 import { writable, derived } from 'svelte/store';
 
 export const swatches = writable([
-	{ name: 'swatch1', lightness: 99, chroma: 0.05, hue: 320 },
-	{ name: 'swatch2', lightness: 90, chroma: 0.1, hue: 320 },
-	{ name: 'swatch3', lightness: 80, chroma: 0.2, hue: 320 },
-	{ name: 'swatch4', lightness: 72, chroma: 0.25, hue: 320 },
-	{ name: 'swatch5', lightness: 67, chroma: 0.31, hue: 320 },
-	{ name: 'swatch6', lightness: 50, chroma: 0.27, hue: 320 },
-	{ name: 'swatch7', lightness: 35, chroma: 0.25, hue: 320 },
-	{ name: 'swatch8', lightness: 25, chroma: 0.2, hue: 320 },
-	{ name: 'swatch9', lightness: 25, chroma: 0.1, hue: 320 },
-	{ name: 'swatch10', lightness: 20, chroma: 0.1, hue: 320 }
+	{ name: 'swatch1', lightness: 94, chroma: 0.04, hue: 320 },
+	{ name: 'swatch2', lightness: 89, chroma: 0.09, hue: 320 },
+	{ name: 'swatch3', lightness: 74, chroma: 0.24, hue: 320 },
+	{ name: 'swatch4', lightness: 67, chroma: 0.31, hue: 320 },
+	{ name: 'swatch5', lightness: 60, chroma: 0.3, hue: 320 },
+	{ name: 'swatch6', lightness: 50, chroma: 0.255, hue: 320 },
+	{ name: 'swatch7', lightness: 35, chroma: 0.18, hue: 320 },
+	{ name: 'swatch8', lightness: 25, chroma: 0.12, hue: 320 },
+	{ name: 'swatch9', lightness: 20, chroma: 0.06, hue: 320 },
+	{ name: 'swatch10', lightness: 13, chroma: 0.06, hue: 320 }
 ]);
 
 export const derivedSwatches = derived(swatches, ($swatches) =>
@@ -21,7 +21,6 @@ export const derivedSwatches = derived(swatches, ($swatches) =>
 	})
 );
 
-
 export const formattedSwatches = derived(derivedSwatches, ($derivedSwatches) =>
-	$derivedSwatches.map((swatch, index) => `--swatch-${index+1}: ${swatch.colour};`)
+	$derivedSwatches.map((swatch, index) => `--swatch-${index + 1}: ${swatch.colour};`)
 );
